b484f12e41f002f2f702360bcbd8c576872eee5a
Mehmet Fatih Yıldız <fatih@mfyz.com>
2020-12-31 12:20:24 -0500

    added iphone web app capable metas

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,9 @@ const Layout = (props) => (
 	<div className={`layout ${props.className}`}>
 		<Head>
         	<title>Speaker Timer</title>
+			<meta name="apple-mobile-web-app-capable" content="yes" />
+			<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+			<meta name="viewport" content="viewport-fit=cover, user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" />
 		</Head>
 		<Header />
 		<div className="content">
